89b14301681c814be118f846021c307bb94674d2
Tokuzo4 <93643473+Tokuzo4@users.noreply.github.com>
2021-12-20 10:57:19 +0900

    destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :set_items, except: [:index, :new, :create]
   before_action :authenticate_user!, only: [:new, :edit, :update]
-  before_action :contributor_confirmation, only: [:new, :edit, :update, :destroy]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  before_action :set_items, except: [:index, :new, :create]
 
   def index
     @items = Item.all.order('created_at DESC')
