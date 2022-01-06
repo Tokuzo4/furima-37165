a7d04ad6ca384d8b5ca53a2b8a2764666b6fe10d
Tokuzo4 <93643473+Tokuzo4@users.noreply.github.com>
2022-01-06 18:04:24 +0900

    エラーを日本語で

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,11 +13,11 @@ class Item < ApplicationRecord
   validates :images, length: { minimum: 1, maximum: 5, message: 'は1枚以上5枚以下にしてください' }
   validates :name, presence: true
   validates :explanation, presence: true
-  validates :category_id, presence: true, numericality: { other_than: 1, message: "category can't be blank" }
-  validates :status_id, presence: true, numericality: { other_than: 1, message: "status can't be blank" }
-  validates :postage_id, presence: true, numericality: { other_than: 1, message: "postage can't be blank" }
-  validates :prefecture_id, presence: true, numericality: { other_than: 0, message: "prefecture can't be blank" }
-  validates :days_to_ship_id, presence: true, numericality: { other_than: 1, message: "days_to_ship can't be blank" }
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "を選択してください" }
+  validates :status_id, presence: true, numericality: { other_than: 1, message: "を選択してください" }
+  validates :postage_id, presence: true, numericality: { other_than: 1, message: "を選択してください" }
+  validates :prefecture_id, presence: true, numericality: { other_than: 0, message: "を選択してください" }
+  validates :days_to_ship_id, presence: true, numericality: { other_than: 1, message: "を選択してください" }
   validates :price, presence: true,
                     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
--- app/models/order_history_address.rb
+++ app/models/order_history_address.rb
@@ -6,12 +6,13 @@ class OrderHistoryAddress
     validates :user_id
     validates :item_id
     validates :token
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Postal code は半角文字で記述する必要があります' }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'は半角文字で記述する必要があります' }
+    validates :prefecture_id, numericality: { other_than: 0, message: "を選択して下さい" }
     validates :city
     validates :chome_address
     VALID_TELEPHONE_NUMBER_REGEX = /\A\d{10,11}\z/
     validates :telephone_number, format: { with: VALID_TELEPHONE_NUMBER_REGEX, message: 'は半角数値で記述する必要があります' }
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+
   end
   def save
     order_history = OrderHistory.create(user_id: user_id, item_id: item_id)
